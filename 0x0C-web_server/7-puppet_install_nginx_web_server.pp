09a38887ff0e341a3b8d7fba758dd3dd37e65642
Tishlyy <ekaetteessien28@gmail.com>
2022-09-26 04:00:14 -0700

    Nginx

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,13 +1,19 @@
 # Installing and configuring Nginx using puppet manifest
+#
+exec { 'Update server':
+  command => 'apt update',
+  path    => '/usr/bin:/usr/sbin:/bin'
+}
+
 package { 'nginx':
   ensure  => installed
 }
 
 file_line { 'aaaaa':
-  ensure   => 'present',
-  path     => '/etc/nginx/sites-available/default',
-  after    => 'listen 80 default_server;',
-  line     => 'rewrite ^/redirect_me https://theweirdnerd.tech permanent;'
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'listen 80 default_server;',
+  line    => 'rewrite ^/redirect_me https://theweirdnerd.tech permanent;'
 }
 
 file { '/var/www/html/index.html':
@@ -15,9 +21,8 @@ file { '/var/www/html/index.html':
 }
 
 service { 'nginx':
-  ensure      => running,
-  require     => package["nginx"],
-  listen_port => 80
+  ensure  => running,
+  require => package["nginx"]
 }
 
 exec { 'Start server':
