edf9bc242f064bed19ed3a2ae4eb910a5064ba18
Tishlyy <ekaetteessien28@gmail.com>
2022-09-27 21:56:31 -0700

    Puppet

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,37 @@
+# Nginx server installation and configuration
+
+exec { 'apt-get-update':
+  command => '/usr/bin/apt-get update',
+}
+
+package { 'nginx':
+  ensure  => installed,
+  require => Exec['apt-get-update'],
+}
+
+file_line { 'un':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://theweirdnerd.tech permanent;',
+  require => Package['nginx'],
+}
+
+file_line { 'duo':
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'listen 80 default_server;',
+  line    => 'add_header X-Served-By $hostname;',
+  require => Package['nginx'],
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+  require => Package['nginx'],
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => package['nginx'],
+}
+
