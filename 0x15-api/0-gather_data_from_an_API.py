f652bc126ab58b68badcd43162bc76f9027f64f2
Tishly <ekaetteessien27@gmail.com>
2022-10-20 01:25:56 +0000

    Gather user data from API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+'''
+Python script that, using this REST API, for a given employee ID,\
+returns information about his/her TODO list progress
+'''
+import requests as req
+from sys import argv as arg
+
+def gatherData():
+    '''
+    Gets Data from API
+    '''
+    ListOfUsers = req.get('https://jsonplaceholder.typicode.com/users').json()
+    ListOfTodos = req.get('https://jsonplaceholder.typicode.com/todos').json()
+    TotalTasks = 0
+    TotalCompletedTasks = 0
+    TaskDescription = []
+    for user in ListOfUsers:
+        if user.get('id') == int(arg[1]):
+            Employee = user.get('name')
+            break
+    for todo in ListOfTodos:
+        if todo.get('userId') == int(arg[1]):
+           TotalTasks += 1
+           if todo.get('completed') is True:
+               TotalCompletedTasks += 1
+               TaskDescription.append(todo.get('title'))
+     print('Employee {} is done with tasks({}/{}):'.format(Employee,
+                                                           TotalCompletedTasks,
+                                                           TotalTasks))
+
+    for task in TaskDescription:
+        print('\t {}'.format(task))
+
+if __name__ == '__main__':
+    gatherData()
